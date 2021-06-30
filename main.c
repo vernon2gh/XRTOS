3b9fa838689b0011d72d9bd0bb5623cabaf8ee30
vernon2gh <vernon2gm@gmail.com>
2021-06-30 22:11:00 +0800

    Add: print function

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "uart.h"
+#include "print.h"
 #include "pages.h"
 #include "bytes.h"
 
@@ -8,16 +9,20 @@ void start_kernel(void)
 
     uart_init();
     uart_puts("hello world, XRTOS.\n");
+    printf("hello word, printf\n");
 
     page_init();
     tmp = page_alloc(1);
     *tmp = 0x12345678;
     *(tmp+1) = 0xaa;
+    printf("page: %p = 0x%x\n", tmp, *tmp);
+    printf("page: %p = 0x%x\n", tmp+1, *(tmp+1));
     page_free(tmp);
 
     byte_init();
     tmp = byte_alloc(4);
     *tmp = 0xaabbccdd;
+    printf("byte: %p = 0x%x\n", tmp, *tmp);
     byte_free(tmp, 4);
 
     while(1);
---
+++ print.h
@@ -0,0 +1,10 @@
+#ifndef __PRINT_H__
+#define __PRINT_H__
+
+#include <stddef.h>
+#include <stdarg.h>
+#include "uart.h"
+
+int printf(const char* s, ...);
+
+#endif
