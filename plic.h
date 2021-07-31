e9a316dd4890d5aecafffc79ba44d168439fd735
vernon2gh <vernon2gm@gmail.com>
2021-07-31 15:55:13 +0800

    Fixed: PLIC_PENDING

--- plic.h
+++ plic.h
@@ -7,7 +7,7 @@
 
 #define PLIC_BASE               0x0C000000L
 #define PLIC_PRIORITY(irq)      (irq * 4)
-#define PLIC_PENDING(irq)       (0x1000 + irq / 32)
+#define PLIC_PENDING(irq)       (0x1000 + (irq / 32) * 4)
 #define PLIC_MENABLE(hartid)    (0x2000 + hartid * 0x80)
 #define PLIC_MTHRESHOLD(hartid) (0x200000 + hartid * 0x1000)
 #define PLIC_MCLAIM(hartid)     (0x200004 + hartid * 0x1000)
