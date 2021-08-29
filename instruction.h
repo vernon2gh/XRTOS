2669909f5fe9e59c59c51c04005d75beafb023f5
vernon2gh <vernon2gm@gmail.com>
2021-08-29 11:20:13 +0800

    Fixed: MS to MSTORE and ML to MLOAD

--- instruction.h
+++ instruction.h
@@ -2,13 +2,13 @@
 #define __INSTRUCTION_H__
 
 #ifdef CONFIG_ARCH64
-#define MS              sd
-#define ML              ld
+#define MSTORE          sd
+#define MLOAD           ld
 #define CTX(offset)     (8 * offset)
 #define BYTES           .dword
 #elif CONFIG_ARCH32
-#define MS              sw
-#define ML              lw
+#define MSTORE          sw
+#define MLOAD           lw
 #define CTX(offset)     (4 * offset)
 #define BYTES           .word
 #endif
